878c206889c8807f1f2c9b32cbdd2ccb9dcb8a2d
Thauã Silveira <thaua@outlook.com>
2023-12-13 22:43:22 -0300

    Fix export.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
+import { SimpleServiceAuthorizer } from './implementation/simple-service-authorizer';
 import { ISimpleServiceAuthorizerConfig } from './interfaces/simple-service-authorizer-config.interface';
-import { ISimpleServiceAuthorizer } from './interfaces/simple-service-authorizer.interface';
 
-export { ISimpleServiceAuthorizer, ISimpleServiceAuthorizerConfig };
+export { ISimpleServiceAuthorizerConfig, SimpleServiceAuthorizer };
